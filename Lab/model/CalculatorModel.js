63439918c2a20c007934bc8186f514e8fe71a182
zahidsammy <120346801+zahidsammy@users.noreply.github.com>
2023-12-17 03:58:47 -0800

    Add files via upload

---
+++ Lab/model/CalculatorModel.js
@@ -0,0 +1,8 @@
+const calculatorModel = {
+    add: (a, b) => a + b,
+    subtract: (a, b) => a - b,
+    multiply: (a, b) => a * b,
+    divide: (a, b) => a / b,
+};
+
+module.exports = calculatorModel;
